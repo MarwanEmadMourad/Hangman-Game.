f8c54705f7b3650ddabd2700a26d2ca3792a7181
MarwanEmadMourad <marwanemad36@gmail.com>
2021-10-01 22:15:45 +0200

    using fetch api instead of XMLHTTPRequest also there is an explanation of how the fetch works.

--- requests.js
+++ requests.js
@@ -1,31 +1,27 @@
-// making HTTP (Hyper Text Transfer Protocol) request
+// making HTTP request using fetch instead of XMLhttprequest
+// notice that the fetch api already include promise decleration 
+// so no need to creat a new promise yourself
+// the fetch api returns a promise so it needed to handeled with .then 
+// the response.json() will also returns a promise so we need another .then
+// the last promise when resolves it will give you the direct object so you can access 
+// it's properties as we did with data.puzzle
 
-// notice that this is a wrapper function that return a new promise back
-// we tend to do this as the promise constuctor only takes 2 args only which are resolve and reject
-// so if we need to pass a third argument like in this case wrap the promise inside a function
-
-const getPuzzle = (wordCount) => new Promise ((resolve,reject) =>{
-    const request = new XMLHttpRequest()
-    request.open('GET',`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-    request.send()
-    
-    request.addEventListener('readystatechange',(e)=>{
-        if (e.target.readyState === 4 && e.target.status == 200){
-            const data = JSON.parse(e.target.responseText)
-            resolve(data.puzzle)
-        } else if (e.target.readyState === 4){
-            reject(`Couldn't retrieve data.`)
+const getPuzzle = (wordCount) => {
+    fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response)=>{
+        if (response.status === 200){
+            return response.json()
+        } else {
+            throw ('Cannot fetch puzzle')
         }
+    }).then ((data)=>{
+        console.log(data.puzzle)
+    }).catch((err)=>{
+        console.log(err)
     })
-})
+}
 
-// we are able to call .then on getPuzzle as it returns a promise
-getPuzzle('5').then(
-    (data)=>{
-        console.log(data)
-}).catch((err)=>{
-    console.log(err)
-}) 
-
-
+// just call your get puzzle with your desired word count
+// no need for the .then and .catch in here as it is already
+// had been taken care of by defualt because of the fetch api 
+getPuzzle('1')
 
